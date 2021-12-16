7c2c25fff62cb4bb479ffc0a9e279836d963e22a
ElleElleElle <13708448+Michasa@users.noreply.github.com>
2021-12-16 21:11:11 +0000

    Work thus far

---
+++ hardhat.config.js
@@ -0,0 +1,9 @@
+// this is where you declare your plugins
+
+require("@nomiclabs/hardhat-waffle");
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.7",
+};
