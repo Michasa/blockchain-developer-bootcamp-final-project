2b56691de893539abef2cbef725568747979f58e
Michelle Sam <michelles@eu.square-enix.com>
2022-01-04 23:57:10 +0000

    Implement commitment submission functions and time simulation test

--- contracts/AccountabilityChecker.sol
+++ contracts/AccountabilityChecker.sol
@@ -2,64 +2,76 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract AccountabilityChecker {
-    // ANCHOR  STATE VARIABLES
+    // SECTION  STATE VARIABLES
     uint256 public one_day_in_seconds = 86400; //could I outsource to a different contract?
 
     address public owner;
-    bytes32[] public commitments;
+    bytes32[] private commitments;
     uint256 private pledge_pot;
     uint256 private daily_wager;
 
     uint256 private reward_pot;
     uint256 private penalty_pot;
 
-    bool private isPromiseActive;
-    uint256 private creation_date;
-    uint256 private deadline;
-    uint256 private last_checked;
-    uint256 private days_left;
+    bool public isPromiseActive;
 
-    // ANCHOR EVENTS
+    uint256 public promise_deadline;
+    uint256 public commitments_check_open;
+    uint256 public commitments_check_deadline;
+    uint256 private commitments_last_checked;
+    uint256 private checks_left;
+
+    // SECTION EVENTS
     event promiseSet(
-        uint256 creation_date,
         bytes32[] commitments,
         uint256 pledge_pot,
-        uint256 deadline,
-        uint256 days_left
+        uint256 promise_deadline,
+        uint256 checks_left
     );
-    event potTotals(
+
+    event commitmentsChecked(
         uint256 pledge_pot,
         uint256 reward_pot,
-        uint256 penalty_pot
+        uint256 penalty_pot,
+        uint256 checks_left
     );
-    event cashOutResult(bool hasCashedOut);
 
-    // ANCHOR MODIFIERS
+    event commitmentTimesUpdated(
+        uint256 commitments_check_open,
+        uint256 commitments_check_deadline
+    );
+    // event log(uint256 given, uint256 open);
+    //event cashOutResult(bool hasCashedOut);
+
+    // SECTION MODIFIERS
     modifier isOwner() {
         require(msg.sender == owner, "owner only");
         _;
     }
 
-    // NOTE Might no longer be relevant
-    // modifier potHasEnough() {
-    //     require(daily_wager < pledge_pot, "insufficient balance");
-    //     _;
-    // }
-
-    modifier updatePromiseStatus() {
-        if (block.timestamp > deadline) {
-            isPromiseActive = false;
-        }
-        _;
-    }
-
-    modifier checkPromiseIsActive() {
+    modifier promiseActive() {
         require(isPromiseActive, "promise isnt active");
         _;
     }
 
-    modifier checkPromiseIsntActive() {
-        require(isPromiseActive != true, "promise is active"); //try !isPromiseActive
+    modifier promiseNotActive() {
+        require(!isPromiseActive, "promise is active");
+        _;
+    }
+    modifier commitmentNotSubmitted(uint256 time_given) {
+        require(
+            time_given > commitments_last_checked &&
+                time_given >= commitments_check_open,
+            "already submitted"
+        );
+        _;
+    }
+
+    modifier promiseNotExpired() {
+        require(
+            checks_left != 0 && !(block.timestamp >= promise_deadline),
+            "promise expired; cashout now"
+        );
         _;
     }
 
@@ -67,13 +79,13 @@ contract AccountabilityChecker {
         owner = msg.sender;
     }
 
-    //ANCHOR MAIN FUNCTIONS
+    //SECTION MAIN FUNCTIONS
     // 🌟 CREATE PROMISE
     function activatePromise(
         bytes32[] memory my_commitments,
         uint256 my_wager,
         uint256 my_due_date
-    ) public payable isOwner checkPromiseIsntActive {
+    ) public payable isOwner promiseNotActive {
         require(
             my_commitments.length != 0 && my_commitments.length <= 3,
             "1-3 commitments only"
@@ -81,59 +93,62 @@ contract AccountabilityChecker {
         require(my_due_date > block.timestamp, "future dates only");
         require(
             ((my_due_date - block.timestamp) / one_day_in_seconds) >= 1,
-            "dates > 24hrs away only"
+            "dates >24hrs away only"
         );
-        require(my_wager > 0, "insufficient wager defined");
+        //TODO should prevent time being more than 30 days
+        require(my_wager > 0, "insufficient wager");
         require(
             msg.value >=
                 my_wager *
-                    ((my_due_date - block.timestamp) / one_day_in_seconds),
-            "insufficient pledge pot amount"
+                    (((my_due_date - block.timestamp) / one_day_in_seconds)),
+            "insufficient pledge amount"
         );
 
         pledge_pot = msg.value;
         isPromiseActive = true;
-        creation_date = block.timestamp;
-        deadline = my_due_date;
+        promise_deadline = my_due_date;
 
-        days_left = (deadline - creation_date) / one_day_in_seconds;
-
+        checks_left = (promise_deadline - block.timestamp) / one_day_in_seconds;
         daily_wager = my_wager;
         commitments = my_commitments;
 
-        emit promiseSet(
-            creation_date,
-            commitments,
-            pledge_pot,
-            deadline,
-            days_left
-        );
+        updateCommitmentCheckTimes(block.timestamp);
+        emit promiseSet(commitments, pledge_pot, promise_deadline, checks_left);
     }
 
     // 🔍 CHECK PROMISE
-    // function checkPromise(bool did_user_complete)
-    //     public
-    //     potHasEnough
-    //     isOwner
-    //     checkPromiseIsActive
-    // {
-    //     //NOTE can only checkPromise ONCE every 24hrs.
-    //     //  If 24hrs and no result is submitted automatic deduction to penalty pot.
+    function checkCommitments(bool commitments_fulfiled, uint256 time_submitted)
+        public
+        isOwner
+        promiseActive
+        commitmentNotSubmitted(time_submitted)
+        promiseNotExpired
+    {
+        if (time_submitted < commitments_check_deadline) {
+            pledge_pot -= daily_wager;
+            commitments_fulfiled == true
+                ? reward_pot += daily_wager
+                : penalty_pot += daily_wager;
+        } else {
+            revert("uhoh penalty time");
+        }
 
-    //     pledge_pot -= daily_wager;
-    //     if (did_user_complete) {
-    //         reward_pot += daily_wager;
-    //     } else {
-    //         penalty_pot += daily_wager;
-    //     }
+        commitments_last_checked = block.timestamp;
+        checks_left -= 1;
+        updateCommitmentCheckTimes(commitments_check_open + one_day_in_seconds);
 
-    //     emit potTotals(pledge_pot, reward_pot, penalty_pot);
-    // }
+        emit commitmentsChecked(
+            pledge_pot,
+            reward_pot,
+            penalty_pot,
+            checks_left
+        );
+    }
 
     // 🤑 CASHOUT REWARD
 
     // function cashOutReward() public payable isOwner {
-    //     //NOTE can only cash out once deadline has passed OR inffucient funds
+    //     //NOTE can only cash out once promise_deadline has passed OR inffucient funds
     //     // correct transfer amount
 
     //     uint256 payout = reward_pot + pledge_pot;
@@ -145,38 +160,25 @@ contract AccountabilityChecker {
     //     emit cashOutResult(sent);
     // }
 
-    //ANCHOR UTIL FUNCTIONS
+    //SECTION UTIL FUNCTIONS (move this to a library)
 
-    // //TODO fix this return function
+    function updateCommitmentCheckTimes(uint256 new_time) internal {
+        commitments_check_open = new_time;
+        commitments_check_deadline =
+            commitments_check_open +
+            one_day_in_seconds;
+
+        emit commitmentTimesUpdated(
+            commitments_check_open,
+            commitments_check_deadline
+        );
+    }
     // function showStats() public view returns () {
     //     pledge_pot;
     //     daily_wager;
     //     reward_pot;
     //     penalty_pot;
-    //     deadline;
+    //     promise_deadline;
     //     isPromiseActive;
     // }
-
-    // function checkPromise(bool did_user_complete)
-    //     public
-    //     potHasEnough
-    //     isOwner
-    //     checkPromiseIsActive
-    // {
-    //     //NOTE can only checkPromise ONCE every 24hrs.
-    //     //  If 24hrs and no result is submitted automatic deduction to penalty pot.
-
-    //     pledge_pot -= daily_wager;
-    //     if (did_user_complete) {
-    //         reward_pot += daily_wager;
-    //     } else {
-    //         penalty_pot += daily_wager;
-    //     }
-
-    //     emit potTotals(pledge_pot, reward_pot, penalty_pot);
-    // }
-
-    receive() external payable {}
-
-    fallback() external payable {}
 }
--- test/Initialisation_And_Activation.js
+++ test/Initialisation_And_Activation.js
@@ -1,17 +1,21 @@
 const { expect } = require("chai");
 const truffleAssert = require("truffle-assertions");
+const { BN, time } = require("@openzeppelin/test-helpers");
 const dayjs = require("dayjs");
-require("mocha-steps");
+var isTomorrow = require("dayjs/plugin/isTomorrow");
+dayjs.extend(isTomorrow);
 
 const {
   COMMITMENTS,
-  SUFFICIENT_PLEDGE_AMOUNT,
   DAILY_WAGER,
   DEADLINE,
+  TIME_NOW,
   PASSED_DEADLINE,
   TOO_MANY_COMMITMENTS,
   TOO_SOON_DEADLINE,
-} = require("./variables");
+  DAY_IN_SECONDS,
+} = require("./utils/variables");
+const { returnPledgeAmount } = require("./utils/functions");
 
 const Contract = artifacts.require("AccountabilityChecker");
 
@@ -19,8 +23,8 @@ contract("⭐ Contract Deployment and Initialisation", function async(accounts) 
   const [owner, notOwner] = accounts;
   let AccountabilityChecker;
 
-  before(async function () {
-    AccountabilityChecker = await Contract.deployed();
+  beforeEach(async function () {
+    AccountabilityChecker = await Contract.new();
   });
 
   it("should have an owner", async function () {
@@ -35,132 +39,170 @@ contract("⭐ Contract Deployment and Initialisation", function async(accounts) 
 contract("🛳️ Promise Activation Tests", function (accounts) {
   const [owner, notOwner] = accounts;
   beforeEach(async function () {
-    AccountabilityChecker = await Contract.deployed();
+    AccountabilityChecker = await Contract.new();
   });
 
-  it("should REJECT promise creation if NOT ENOUGH is provided FOR the PLEDGE POT", async function () {
+  it("promise creation should REVERT if NOTHING is added to PLEDGE POT", async function () {
+    await truffleAssert.reverts(
+      AccountabilityChecker.activatePromise(COMMITMENTS, DAILY_WAGER, DEADLINE),
+      "insufficient pledge amount"
+    );
+  });
+
+  it("promise creation should REVERT if NOT ENOUGH is provided FOR the PLEDGE POT", async function () {
     await truffleAssert.reverts(
       AccountabilityChecker.activatePromise(
         COMMITMENTS,
         DAILY_WAGER,
         DEADLINE,
         {
-          value: web3.utils.toBN(1000),
+          value: new BN(1000),
         }
       ),
-      "insufficient pledge pot amount"
+      "insufficient pledge amount"
     );
   });
 
-  it("should REJECT promise creation if NO DAILY WAGER is defined", async function () {
+  it("promise creation should REVERT if NO DAILY WAGER is defined", async function () {
     await truffleAssert.reverts(
-      AccountabilityChecker.activatePromise(
-        COMMITMENTS,
-        web3.utils.toBN(0),
-        DEADLINE,
-        {
-          value: SUFFICIENT_PLEDGE_AMOUNT,
-        }
-      ),
-      "insufficient wager defined"
+      AccountabilityChecker.activatePromise(COMMITMENTS, new BN(0), DEADLINE, {
+        value: returnPledgeAmount(DEADLINE),
+      }),
+      "insufficient wager"
     );
   });
 
-  it("should REJECT promise creation if NO COMMITMENTS ARE PROVIDED", async function () {
+  it("promise creation should REVERT if NO COMMITMENTS ARE PROVIDED", async function () {
     await truffleAssert.reverts(
       AccountabilityChecker.activatePromise([], DAILY_WAGER, DEADLINE, {
-        value: SUFFICIENT_PLEDGE_AMOUNT,
+        value: returnPledgeAmount(DEADLINE),
       }),
       "3 commitments only"
     );
   });
 
-  it("should REJECT promise creation if MORE THAN 3 COMMITMENTS ARE PROVIDED", async function () {
+  it("promise creation should REVERT if MORE THAN 3 COMMITMENTS ARE PROVIDED", async function () {
     await truffleAssert.reverts(
       AccountabilityChecker.activatePromise(
         TOO_MANY_COMMITMENTS,
         DAILY_WAGER,
         DEADLINE,
         {
-          value: SUFFICIENT_PLEDGE_AMOUNT,
+          value: returnPledgeAmount(DEADLINE),
         }
       ),
       "3 commitments only"
     );
   });
 
-  it("should REJECT promise creation if GIVEN DEADLINE HAS PASSED", async function () {
+  it("promise creation should REVERT if GIVEN DEADLINE HAS PASSED", async function () {
     await truffleAssert.reverts(
       AccountabilityChecker.activatePromise(
         COMMITMENTS,
         DAILY_WAGER,
         PASSED_DEADLINE,
         {
-          value: SUFFICIENT_PLEDGE_AMOUNT,
+          value: returnPledgeAmount(DEADLINE),
         }
       ),
       "future dates only"
     );
   });
 
-  it("should REJECT promise creation if GIVEN DEADLINE IS TOO SOON", async function () {
+  it("promise creation should REVERT if GIVEN DEADLINE IS TOO SOON", async function () {
     await truffleAssert.reverts(
       AccountabilityChecker.activatePromise(
         COMMITMENTS,
         DAILY_WAGER,
         TOO_SOON_DEADLINE,
         {
-          value: SUFFICIENT_PLEDGE_AMOUNT,
+          value: returnPledgeAmount(TOO_SOON_DEADLINE),
         }
       ),
-      "dates > 24hrs away only"
+      "dates >24hrs away only"
     );
   });
 
-  step(
-    "should ACCEPT creation if ALL THE CORRECT VARIABLES ARE PROVIDED...",
-    async function () {
-      let result = await AccountabilityChecker.activatePromise(
-        COMMITMENTS,
-        DAILY_WAGER,
-        DEADLINE,
-        {
-          value: SUFFICIENT_PLEDGE_AMOUNT,
-        }
-      );
-      truffleAssert.eventEmitted(result, "promiseSet");
-    }
-  );
-
-  step(
-    "...but should REJECT creation if user submits again but PROMISE IS ALREADY ACTIVE.",
-    async function () {
-      await truffleAssert.reverts(
-        AccountabilityChecker.activatePromise(
-          COMMITMENTS,
-          DAILY_WAGER,
-          DEADLINE,
-          {
-            value: SUFFICIENT_PLEDGE_AMOUNT,
-          }
-        ),
-        "promise is active"
-      );
-    }
-  );
-
-  it("should REJECT creation is NOT FROM OWNER...", async function () {
+  it("promise creation should REVERT is NOT FROM OWNER...", async function () {
     await truffleAssert.reverts(
       AccountabilityChecker.activatePromise(
         COMMITMENTS,
         DAILY_WAGER,
         DEADLINE,
         {
-          value: SUFFICIENT_PLEDGE_AMOUNT,
+          value: returnPledgeAmount(DEADLINE),
           from: notOwner,
         }
       ),
       "owner only"
+    );
+  });
+
+  it("should REJECT promise submission if PROMISE NOT ACTIVE", async function () {
+    await truffleAssert.reverts(
+      AccountabilityChecker.checkCommitments(true, TIME_NOW),
+      "promise isnt active"
+    );
+  });
+
+  it("should ACCEPT creation if ALL THE CORRECT VARIABLES ARE PROVIDED", async function () {
+    let result = await AccountabilityChecker.activatePromise(
+      COMMITMENTS,
+      DAILY_WAGER,
+      DEADLINE,
+      {
+        value: returnPledgeAmount(DEADLINE),
+      }
+    );
+
+    let BLOCK_TIMESTAMP = (await time.latest()).toNumber();
+    let BLOCK_TIMESTAMP_24HRS_FROM_NOW = BLOCK_TIMESTAMP + DAY_IN_SECONDS;
+
+    truffleAssert.eventEmitted(
+      result,
+      "promiseSet",
+      ({ commitments, pledge_pot, promise_deadline }) => {
+        return (
+          expect(commitments).to.eql(COMMITMENTS) &&
+          pledge_pot.toNumber() === returnPledgeAmount(DEADLINE).toNumber() &&
+          promise_deadline.toNumber() === DEADLINE
+        );
+      }
+    );
+
+    truffleAssert.eventEmitted(
+      result,
+      "commitmentTimesUpdated",
+      ({ commitments_check_deadline, commitments_check_open }) => {
+        return (
+          BLOCK_TIMESTAMP === commitments_check_open.toNumber() &&
+          BLOCK_TIMESTAMP_24HRS_FROM_NOW ===
+            commitments_check_deadline.toNumber()
+        );
+      }
+    );
+  });
+
+  it("should REJECT creation if user submits again but PROMISE IS ALREADY ACTIVE.", async function () {
+    let result = await AccountabilityChecker.activatePromise(
+      COMMITMENTS,
+      DAILY_WAGER,
+      DEADLINE,
+      {
+        value: returnPledgeAmount(DEADLINE),
+      }
+    );
+    if (!result) return done(new Error("No result returned"));
+    await truffleAssert.reverts(
+      AccountabilityChecker.activatePromise(
+        COMMITMENTS,
+        DAILY_WAGER,
+        DEADLINE,
+        {
+          value: returnPledgeAmount(DEADLINE),
+        }
+      ),
+      "promise is active"
     );
   });
 });
